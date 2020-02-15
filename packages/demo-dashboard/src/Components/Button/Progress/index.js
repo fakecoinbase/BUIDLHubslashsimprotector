5d91a2d86b7b82ed88db2608ee8a9e82d801f122
Mitchell Opatowsky <opatowsky77@gmail.com>
2020-02-15 12:31:40 -0700

    card view + nav start

---
+++ packages/demo-dashboard/src/Components/Button/Progress/index.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Button } from "reactstrap";
+import cn from "classnames";
+
+export default class ButtonProgress extends React.Component {
+  render() {
+    let { className, small, tiny, size } = this.props;
+
+    if (small) {
+      size = "sm";
+    }
+    if (tiny) {
+      size = "xs";
+    }
+    if (!size) {
+      size = "md";
+    }
+
+    return (
+      <Fragment>
+        <Button
+          size={size}
+          onClick={this.props.onClick}
+          className={cn(className, "button-progress")}
+        >
+          {this.props.children}
+        </Button>
+      </Fragment>
+    );
+  }
+}
