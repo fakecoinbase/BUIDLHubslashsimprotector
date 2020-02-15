cf9615f6aab518196c56665acbeb2d6d5d5a022d
mdcoon <mdcoon1@yahoo.com>
2020-02-15 15:02:42 -0500

    Added animated step wizard

---
+++ packages/demo-dashboard/src/Components/StepWizard/index.js
@@ -0,0 +1,112 @@
+import cn from 'classnames';
+import * as align from 'Constants/alignments';
+import { 
+    Row, 
+    Col,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Button 
+} from 'reactstrap';
+import {tryCall} from 'Utils';
+import React, { Component } from 'react'
+
+import StepWizard from 'react-step-wizard';
+
+/**
+ * Turns child elements into an animated set of steps
+ */
+export default class StepWizardContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 0
+        };
+        this.wizardRef = React.createRef();
+    }
+
+    next = () => {
+        let n = this.state.page + 1
+        if(n >= this.props.stepMeta.length)  {
+            n = this.props.stateMeta.length-1;
+        }
+        if(this.wizardRef.current.nextStep) {
+            this.wizardRef.current.nextStep();
+            this.setState({
+                page: n
+            })
+        }
+    }
+
+    prev = () => {
+        let n = this.state.page - 1;
+        if(n < 0) {
+            n = 0;
+        }
+
+        if(this.wizardRef.current.previousStep) {
+            this.wizardRef.current.previousStep();
+            this.setState({
+                page: n
+            })
+        }
+    }
+
+    render() {
+        
+        const {
+            stepMeta
+        } = this.props;
+
+        let page = this.state.page;
+        let meta = stepMeta[page];
+        let dirty = meta.dirty;
+        let hasNext = page < (stepMeta.length - 1) && !dirty;
+        let hasPrev = page > 0;
+
+        return (
+            <div className={cn(align.full, align.allCenter, align.noMarginPad)}>
+                <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
+                    <Col xs="10" className={cn(align.allCenter, align.noMarginPad)}>
+                        <Card className={cn("step-card", align.full, align.noMarginPad)}>
+                            <CardHeader className={cn(align.full, "step-header")}>
+                                <div className={cn(align.full, align.leftCenter, align.noMarginPad)}>
+                                    <span className={cn('font-weight-bold', 'text-1')}>
+                                        {meta.title}
+                                    </span>
+                                </div>
+                            </CardHeader>
+
+
+                            <CardBody className={cn("step-body", align.full, align.noMarginPad)}>
+
+                                <StepWizard ref={this.wizardRef}>
+                                    {this.props.children}
+                                </StepWizard>
+
+                            </CardBody>
+
+                            <CardFooter className={cn(align.full, align.rightCenter, align.noMarginPad)}>
+                                <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
+                                    <Col xs="6" className={cn(align.leftCenter, align.noMarginPad)}>
+                                        <Button size="sm" color={hasPrev?"primary":"secondary"}
+                                                onClick={this.prev}
+                                                disabled={!hasPrev}>Prev</Button>
+                                    </Col>
+
+                                    <Col xs="6" className={cn(align.rightCenter, align.noMarginPad)}>
+                                        <Button size="sm" color={hasNext?"primary":"secondary"}
+                                                 onClick={this.next}
+                                                disabled={!hasNext}>Next</Button>
+                                    </Col>
+                                </Row> 
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
--- packages/demo-dashboard/src/Views/manufacturer/Manufacturer.jsx
+++ packages/demo-dashboard/src/Views/manufacturer/Manufacturer.jsx
@@ -1,21 +1,71 @@
 import cn from 'classnames';
 import * as align from 'Constants/alignments';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Input, Button } from 'reactstrap';
 import React, { Component } from 'react'
 import Select from 'react-select';
+import Loading from 'Components/Loading';
+import {tryCall} from 'Utils';
+import QRReader from 'qrcode.react';
+
+import StepWizard from 'Components/StepWizard';
 
 export default class Manufacturer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selectedFn: null,
+            paramData: [],
+            dirty: false,
+            loading: false,
+            qrCodeValue: null,
+            qrCodeError: null
         }
     }
 
     onFnSel = sel => {
         this.setState({
-            selectedFn: sel
+            selectedFn: sel.value,
+            dirty: true,
+            paramData: []
         })
+    }
+
+    paramChanged = (idx, val) => {
+        let paramData = [
+            ...this.state.paramData
+        ];
+        paramData[idx] = val;
+        this.setState({
+            dirty: true,
+            paramData
+        })
+    }
+
+    generateCode = () => {
+        this.setState({
+            dirty: false,
+            loading: true
+        }, async () => {
+            try {
+                
+                let r = await tryCall(this.props.generateQRCode, {
+                    fn: this.state.selectedFn.actualFn,
+                    params: this.state.paramData
+                });
+                console.log("Signed data", r);
+                
+                this.setState({
+                    loading: false,
+                    qrCodeValue: r
+                });
+            } catch (e) {
+                console.log(e);
+                this.setState({
+                    loading: false,
+                    qrCodeError: e.message
+                });
+            }
+        });
     }
 
     render() {
@@ -23,20 +73,72 @@ export default class Manufacturer extends Component {
             functions
         } = this.props;
 
-        let opts = functions.map(f=>{
-           return {
-                label: f.name,
-                value: f
+        let dirty  = this.state.dirty;
+        
+        let steps = [
+            {
+                title: "Txn Info",
+                dirty
+            },
+            {
+                title: "2nd page"
             }
-        });
-
-        let ins = [];
-        if(this.state.selectedFn) {
-            ins = this.state.selectedFn.inputs
-        }
+        ]
 
         return (
-            <div className={cn(align.full, align.topCenter, align.noMarginPad)}>
+            <StepWizard stepMeta={steps}>
+                <MainPage  {...this.props} 
+                            stateData={this.state} 
+                            onFnSel={this.onFnSel} 
+                            generateCode={this.generateCode} 
+                            paramChanged={this.paramChanged} />
+                
+                <div className={cn(align.full, align.allCenter, align.noMarginPad)}>
+                    <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
+                        <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
+                            <span className={cn('font-weight-light', 'text-1')}>
+                                Page 2
+                            </span>
+                        </Col>
+                    </Row>
+                </div>
+            </StepWizard>
+        )
+    }
+}
+
+const MainPage = props => {
+    const {
+        functions,
+        onFnSel,
+        generateCode,
+        paramChanged,
+        stateData
+    } = props;
+
+    let loading = stateData.loading;
+    let vals = stateData.paramData;
+    let qrData  = stateData.qrCodeValue;
+    let qrErr = stateData.qrCodeError;
+    let selectedFn = stateData.selectedFn;
+    let dirty = stateData.dirty;
+
+    let opts = functions.map(f=>{
+        return {
+             label: f.name,
+             value: f
+         }
+     });
+     
+    let ins = [];
+    if(selectedFn) {
+        ins = selectedFn.inputs
+    }
+
+    return (
+        <div className={cn(align.full, align.topCenter, align.noMarginPad)}>
+               <Loading loading={loading} />
+
                 <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
                     <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
                         Select function to encode
@@ -45,45 +147,67 @@ export default class Manufacturer extends Component {
                 <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
                     <Col xs="6" className={cn(align.allCenter, align.noMarginPad)}>
                         <Select className={cn(align.full)} 
-                                options={opts} onChange={this.onFnSel} />
+                                options={opts} onChange={onFnSel} />
                     </Col>
                 </Row>
                 {
                     ins.length > 0 &&
                     <React.Fragment>
-                        <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
-                            <Col xs="6" className={cn(align.allCenter, align.noMarginPad)}>
-                                <span className={cn('font-weight-light', 'text-1')}>
+                        <Row className={cn(align.full, "pt-5", align.noMarginPad, align.allCenter)}>
+                            <Col xs="6" className={cn(align.leftCenter, align.noMarginPad)}>
+                                <span className={cn('font-weight-bold', 'text-1')}>
                                     Input Params
                                 </span>
                             </Col>
                         </Row>
-                        <div className={cn(align.full, align.topCenter, align.noMarginPad)}>
-                            
-                        {
-                            ins.map( (f, i) => {
-                                return (
-                                    <Row key={i} className={cn(align.full, align.noMarginPad, align.topCenter)}>
-                                        <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
-                                            <span className={cn('font-weight-light', 'text-1')}>
-                                                Param {f.name}
-                                            </span>
-                                        </Col>
-                                        <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
-                                            <span className={cn('font-weight-light', 'text-1')}>
-                                                Param {f.name}
-                                            </span>
-                                        </Col>
-                                    </Row>
-                                )
-                            })
-                        }
-                        </div>
+
+                        <Row className={cn(align.full, align.noMarginPad, align.allCenter)}>
+                            <Col xs="6" className={cn(align.allCenter, align.noMarginPad)}>
+                                
+                                <div className={cn(align.full, align.topCenter, align.noMarginPad)}>
+                                    {
+                                        ins.map( (f, i) => {
+                                            return (
+                                                <Row key={i} className={cn(align.full, align.noMarginPad, align.topCenter)}>
+                                                    <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
+                                                        <span className={cn('font-weight-light', 'text-1')}>
+                                                            Param {f.name}
+                                                        </span>
+                                                    </Col>
+                                                    <Col xs="12" className={cn(align.allCenter, align.noMarginPad)}>
+                                                        <Input className={cn(align.full)} 
+                                                                onChange={e=>tryCall(paramChanged, i, e.target.value)}
+                                                                value={vals[i] || ''} />
+                                                    </Col>
+                                                </Row>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </Col>
+                        </Row>
                     </React.Fragment>
                 }
 
+                <Row className={cn(align.full, "py-4", align.noMarginPad, align.allCenter)}>
+                    <Col xs="6" className={cn(align.allCenter, align.noMarginPad)}>
+                        <Button size="sm" color={dirty?"primary":"secondary"}
+                            disabled={!dirty} 
+                             onClick={generateCode}>Generate QR Code</Button>
+                    </Col>
+                </Row>
+
+                {
+                    qrData &&
+                    <Row className={cn(align.full, "pt-5", align.noMarginPad, align.allCenter)}>
+                        <Col xs="4" className={cn(align.allCenter, align.noMarginPad)}>
+                            <QRReader value={qrData} />
+                        </Col>
+                    </Row>
+                }
+
+
             </div>
-        )
-    }
+    )
 }
 
